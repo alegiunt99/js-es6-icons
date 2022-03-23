888fc1c5c06e90149d297c5976fdd4a9f562effd
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-23 15:45:57 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('JS OK!')
+
+/* 1. Partendo dalla struttura dati fornita, 
+visualizzare in pagina un box per ogni icona,
+in cui è presente il nome dell'icona e l'icona stessa.*/
+
+/* 2.  Ciascuna icona ha una proprietà "color": 
+utilizzare questa proprietà per visualizzare le
+icone del colore corrispondente.*/
+
+/* 3.  Aggiungere alla pagina una select in cui le 
+options corrispondono ai vari tipi di icone (animal, vegetable, user).
+Quando l'utente seleziona un tipo dalla select, 
+visualizzare solamente le icone corrispondenti.*/
+
